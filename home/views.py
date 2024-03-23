ffc4fa9b79ebd636718eed6f8641ca4345247451
Arash <arashkarimi1999@gmail.com>
2024-03-23 07:59:02 +0330

    add update post

--- home/views.py
+++ home/views.py
@@ -1,8 +1,12 @@
+from typing import Any
+from django.http import HttpRequest
 from django.shortcuts import render, redirect
 from django.views import View
 from .models import Post
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
+from .forms import PostUpdateForm
+from django.utils.text import slugify
 
 
 class HomeView(View):
@@ -18,6 +22,7 @@ class PostDetailView(View):
         post = Post.objects.get(id=post_id, slug=post_slug)
         return render(request, 'home/detail.html', {"post": post})
 
+
 class PostDeleteView(LoginRequiredMixin, View):
 
     def get(self, request, post_id):
@@ -28,4 +33,34 @@ class PostDeleteView(LoginRequiredMixin, View):
         else:
             messages.success(request, "You can't delete this post.", "danger")
         return redirect("home:home")
+
+
+class PostUpdateView(LoginRequiredMixin, View):
+    form_class = PostUpdateForm
+
+    def setup(self, request, *args, **kwargs):
+        self.post_instanse = Post.objects.get(id=kwargs["post_id"])
+        return super().setup(request, *args, **kwargs)
+
+    def dispatch(self, request, *args, **kwargs):
+        post = self.post_instanse
+        if post.user.id != request.user.id:
+            messages.success(request, "You can't update this post.", "danger")
+            return redirect("home:home")
+        return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
+        post = self.post_instanse
+        form = self.form_class(instance=post)
+        return render(request, "home/update.html", {"form": form})
+
+    def post(self, request, *args, **kwargs):
+        post = self.post_instanse
+        form = self.form_class(request.POST, instance=post)
+        if form.is_valid():
+            new_post = form.save(commit=False)
+            new_post.slug = slugify(form.cleaned_data["body"][:30])
+            new_post.save()
+            messages.success(request, "You updated this post.", "success")
+            return redirect("home:post_detail", post.id, post.slug)
 
